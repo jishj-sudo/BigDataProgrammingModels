f38d5cb4a2abfedae17856114d54dcc3820d3e78
Jishnu Jagath <jkj5370@psu.edu>
2022-09-14 15:00:57 -0400

    fixed merge conflict :

--- pythonhw/hw1.py
+++ pythonhw/hw1.py
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 def q1(mystring):
     """ split the string by tabs to get an array and return the array """
     return mystring.split("\t");       
@@ -8,38 +7,20 @@ def q2(mystring):
     """ split the string by tabs to get an array and return the second element of the array """
     split_mystring = q1(mystring);
     return split_mystring[1];
-=======
-
-def q1(mystring):
-    """ split the string by tabs to get an array and return the array """
-    pass
-
-
-def q2(mystring):
-    """ split the string by tabs to get an array and return the second element of the array """
-    pass
-
->>>>>>> 48148a52da64c36ecbbefd2262446d9cd3171795
 
 
 def q3(myarray):
     """ myarray is an list of pairs. this function should return the sum of the first
     items in the pair and the sum of the second items """
-<<<<<<< HEAD
     first,second = 0,0;
     for elem in myarray:
         first += elem[0];
         second += elem[1];
     return (first , second);
-=======
-    pass
-
->>>>>>> 48148a52da64c36ecbbefd2262446d9cd3171795
 
 def q4(mystringarray):
     """ return the position of the first occurrence of the string 'hi' or -1 if it is not found.
     you cannot change how the array is iterated and you cannot use any list operations on mystringarray"""
-<<<<<<< HEAD
     position = 0;
     for mystring in mystringarray:
         if(mystring == "hi"):
@@ -58,13 +39,4 @@ def q5(myarray):
         else:    
             dic[letter] = 1;
     return dic;   
-=======
 
-    for mystring in mystringarray:
-        pass
-
-
-def q5(myarray):
-    """ return a dictionary containing the counts of items in the input array """
-    pass
->>>>>>> 48148a52da64c36ecbbefd2262446d9cd3171795
