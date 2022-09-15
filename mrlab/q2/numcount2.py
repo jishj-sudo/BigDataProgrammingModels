722432e6d5220aada45d6ce562b49980775de166
Jishnu Jagath <jkj5370@psu.edu>
2022-09-15 15:56:44 -0400

    completed q2

---
+++ mrlab/q2/numcount2.py
@@ -0,0 +1,15 @@
+from mrjob.job import MRJob   # MRJob version
+
+# Change the class name!!
+class WordCount(MRJob):  #MRJob version
+    def mapper(self, key, line):
+        words = line.split()
+        for w in words:
+            if (len(w) > 2): # only include words greater than length 2    
+                yield (len(w), 1) 
+
+    def reducer(self, key, values):
+        yield (key, sum(values))
+
+if __name__ == '__main__':
+    WordCount.run()   # MRJob version
