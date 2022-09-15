a6b13072650fc793cf157af39d555d7895a7be5b
Jishnu Jagath <jkj5370@psu.edu>
2022-09-15 16:01:07 -0400

    completed q1

--- mrlab/q1/numcount.py
+++ mrlab/q1/numcount.py
@@ -1,14 +1,14 @@
 from mrjob.job import MRJob   # MRJob version
 
 # Change the class name!!
-class WordCount(MRJob):  #MRJob version
+class WordLengthCount0(MRJob):  #MRJob version
     def mapper(self, key, line):
         words = line.split()
         for w in words:
-            yield (w, 1)
+            yield (len(w), 1) #key is now length of word.
 
     def reducer(self, key, values):
         yield (key, sum(values))
 
 if __name__ == '__main__':
-    WordCount.run()   # MRJob version
+    WordLengthCount0.run()   # MRJob version
